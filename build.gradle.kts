849735717737ae209dd009da380c097a2f2559a7
JayaSuryaT <jayasurya.dc@gmail.com>
2023-07-19 00:11:28 +0530

    (#4) Setup binary-compatibility-validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.diffplug.spotless") version "6.3.0"
     id("org.jetbrains.kotlin.jvm") version "1.8.0" apply false
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.13.2"
 }
 
 apply {
